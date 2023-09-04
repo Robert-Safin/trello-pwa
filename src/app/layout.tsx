b6ae13ecc06340291343d968cb9aafb8beca90ee
Robert Safin <robertsafin98@gmail.com>
2023-09-04 21:20:18 +0800

    re

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
       </head>
-      <body id="root" className="min-h-screen min-w-full max-h-screen max-w-full bg-primary dark:bg-primaryDark">
+      <body
+        id="root"
+        className="min-h-screen min-w-full max-h-screen max-w-full bg-primary dark:bg-primaryDark"
+      >
         <Provider store={store}>
           <PersistGate persistor={persistor}>{children}</PersistGate>
         </Provider>
